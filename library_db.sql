37d816755153680a57635b65eaabc89734cddab1
Kreshanloyd <s.tonson.christianlloyd@cmu.edu.ph>
2026-05-07 23:13:02 +0800

    Add files via upload

--- library_db.sql
+++ library_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 25, 2026 at 03:51 PM
+-- Generation Time: May 07, 2026 at 05:12 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -41,10 +41,16 @@ CREATE TABLE `books` (
 INSERT INTO `books` (`id`, `title`, `author`, `status`) VALUES
 (1, 'Formula 1', 'Max Verstappen', 'Available'),
 (2, 'Formula 2', 'Lewis Hamilton', 'Borrowed'),
-(3, 'Formula 3', 'Charles Leclerc', 'Available'),
-(4, 'Formula 4', 'George Russel', 'Borrowed'),
-(5, 'Formula 5', 'Carlos Sainz', 'Available'),
-(6, 'Formula E', 'Oscar Piastri', 'Borrowed');
+(3, 'Formula 3', 'Charles Leclerc', 'Borrowed'),
+(4, 'GT World Challenge', 'George Russel', 'Borrowed'),
+(5, 'World Rally Championship', 'Carlos Sainz', 'Borrowed'),
+(6, 'Formula E', 'Oscar Piastri', 'Borrowed'),
+(7, '24 Hours of Le Mans', 'Kimi Antonelli', 'Available'),
+(8, 'IndyCar Series', 'Max Verstappen', 'Borrowed'),
+(9, 'Super Formula', 'Lewis Hamilton', 'Available'),
+(10, 'NASCAR Cup Series', 'Charles Leclerc', 'Borrowed'),
+(11, 'British Touring Car Championship', 'Checo Perez', 'Available'),
+(12, 'World Touring Car Cup', 'Carlos Sainz', 'Borrowed');
 
 -- --------------------------------------------------------
 
@@ -67,7 +73,10 @@ CREATE TABLE `borrow_records` (
 
 INSERT INTO `borrow_records` (`id`, `user_name`, `book_id`, `borrow_date`, `return_date`, `status`) VALUES
 (1, 'Kaizen', 1, '2026-04-25', NULL, 'Borrowed'),
-(2, 'Kaizen', 1, '2026-12-12', NULL, 'Borrowed');
+(2, 'Kaizen', 1, '2026-12-12', NULL, 'Borrowed'),
+(3, 'Sho', 1, '2026-05-05', NULL, 'Borrowed'),
+(4, 'Bro', 5, '2026-05-07', NULL, 'Borrowed'),
+(5, 'akay', 3, '2026-05-07', NULL, 'Borrowed');
 
 -- --------------------------------------------------------
 
@@ -87,8 +96,23 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`) VALUES
-(3, 'Kaizen', 'Kaizen@gmail.com', '12345'),
-(4, 'Alan', 'Alan@gmail.com', '12345');
+(1, 'Kaizen', 'Kaizen@gmail.com', '$2y$10$LKFSQlpDLGdxEDBXIy33g.t04FwF.kofNvpP9yKprG/SEAbiBnlJq'),
+(2, 'Alan', 'Alan@gmail.com', '$2y$10$ikTbe/3YxKYhe2N.G4TDz.XSedMDfkqARmMiluO3j5/K9AQHJfPAi'),
+(3, 'Hisen', 'Hazen@gmail.com', '$2y$10$q2/xCUW8IFSqoDeB9DEi0e8EXORWjiFVRgJOhdUGYdCqCvxlKDpAC'),
+(4, 'Debon', 'Devon@gmail.com', '$2y$10$TjK3AK1W/OU/jNdIcD0zVuv2.c0LsnwFbm/UeqqC5eW1mGdOju8BK'),
+(5, 'Sho', 'Sho@gmail.com', '$2y$10$EHeZS1hgDOnfKQ/IRS6JtOi/Hy5pBvNQXDXSxMHRI7NSHAAtxjpg2'),
+(6, 'Vrayl', 'Vrayl@gmail.com', '$2y$10$PrNj3kTipHM.GbtHnFmBBOFdYycJCFpYLYqiwGlCDv.qV/CpfTcya'),
+(7, 'Frenks', 'Frenks@gmail.com', '$2y$10$sPq53MnsOwxVRuDDd0CIdedePIAh4OTH96beH4ptsUhEk/GE2FAty'),
+(13, 'Natnat', 'Natnat@gmail.com', '$2y$10$FDS/ZPLmLQaU69uluxPeUe8SCYpTbgaalVyQ9UUVORNkD0NFcQTlq'),
+(14, 'Ayban', 'Ayban@gmail.com', '$2y$10$oI4CFUMqyxHt8SFbqXhIiez.qKATieuLzjYn9WZURhe9FyB27fkVu'),
+(15, 'John', 'John@gmail.com', '$2y$10$YNRGk79DtgPOoL2q9BARn.nxPBM5Wf4iilSN6.1.tRkLte1U3yS7a'),
+(16, 'Earl', 'Earl@gmail.com', '$2y$10$LwX3Nb8QA034vqsAa1tWj.PNbOBeGr0UpPOaDLE0hY0KPn12aCsfy'),
+(17, 'Kai', 'Kai@gmail.com', '$2y$10$uj9aSdJxIKtdf3vr3zn6ne6vuWfvFY1iXjhaK.cKmiEKLNIwKFtxq'),
+(18, 'Luffy', 'Luffy@gmail.com', '$2y$10$.YErb/hLyiSR2lsFwyVyaeWQZUyC6tr0q/XrDwRjyWZ/OzgN2LbpO'),
+(19, 'Iskay', 'Iskay@gmail.com', '$2y$10$Z1.Wh4ShQUoDjwQQDoLpYupkdNqoOGqaZiQramrK.YiyqJHgQQF2C'),
+(20, 'Sukuna', 'Sukuna@gmail.com', '$2y$10$2KedhqeawFATTFz/qqWL2OXRk40cCYKW0BBNNXzWrdGFIa1yKiXxG'),
+(21, 'Gojo', 'Gojo@gmail.com', '$2y$10$Nv4y59gh5ybK1FUZdwE4E.LlVFy0GmwRdGOQWISZy4LkF7DMkhl5C'),
+(22, 'Bro', 'Bro@gmail.com', '$2y$10$gMCD1d.S6jTIO9p6l.wxjeAcIgvQjo.V6weuYXi8ybbVU3SrdDnhW');
 
 --
 -- Indexes for dumped tables
@@ -120,19 +144,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `books`
 --
 ALTER TABLE `books`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `borrow_records`
 --
 ALTER TABLE `borrow_records`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
